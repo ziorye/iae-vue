438547dc8f6efa5ff87b34302e8012f339711d91
ziorye <ziorye@gmail.com>
2023-11-16 15:04:14 +0800

    step-10: Watchers

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, watch } from 'vue'
 
-const pElementRef = ref(null)
+const todoId = ref(1)
+const todoData = ref(null)
 
-onMounted(() => {
-  pElementRef.value.textContent = 'mounted!'
-})
+async function fetchData() {
+  todoData.value = null
+  const res = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${todoId.value}`
+  )
+  todoData.value = await res.json()
+}
+
+fetchData()
+
+watch(todoId, fetchData)
 </script>
 
 <template>
-  <p ref="pElementRef">hello</p>
+  <p>Todo id: {{ todoId }}</p>
+  <button @click="todoId++">Fetch next todo</button>
+  <p v-if="!todoData">Loading...</p>
+  <pre v-else>{{ todoData }}</pre>
 </template>
