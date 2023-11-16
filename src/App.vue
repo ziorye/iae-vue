85ab7405ef28f5fccdbf36dc38c898696bc9e287
ziorye <ziorye@gmail.com>
2023-11-16 14:35:49 +0800

    step-4: Event Listeners

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <script setup>
 import { ref } from 'vue'
 
-const titleClass = ref('title')
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
 </script>
 
 <template>
-  <h1 :class="titleClass">Make me red</h1>
+  <button @click="increment">count is: {{ count }}</button>
 </template>
-
-<style>
-.title {
-  color: red;
-}
-.title-blue {
-  color: blue;
-}
-</style>
