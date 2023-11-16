f2831055ed5d1101e5a283e8ecc145687c3ee37b
ziorye <ziorye@gmail.com>
2023-11-16 15:24:37 +0800

    step-14: Slots

--- src/ChildComp.vue
+++ src/ChildComp.vue
@@ -1,11 +1,9 @@
 <script setup>
-const emit = defineEmits(['response'])
 
-emit('response', 'hello from child')
 </script>
 
 <template>
-  <h2>Child component</h2>
+  <slot>Fallback content</slot>
 </template>
 
 <style scoped>
