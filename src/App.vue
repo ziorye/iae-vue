49f5f56f0be0bb45373afb2a4da141fa2c1dec50
ziorye <ziorye@gmail.com>
2023-11-16 15:18:16 +0800

    step-12: Props

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+import { ref } from 'vue'
 import ChildComp from './ChildComp.vue'
+
+const greeting = ref('Hello from parent')
 </script>
 
 <template>
-  <ChildComp />
+  <ChildComp :msg="greeting" />
 </template>
--- src/ChildComp.vue
+++ src/ChildComp.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+const props = defineProps({
+  msg: String
+})
 </script>
 
 <template>
-  <h2>A Child Component!</h2>
+  <h2>{{ msg || 'No props passed yet' }}</h2>
 </template>
 
 <style scoped>
